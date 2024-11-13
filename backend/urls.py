92c93f1ed664fc2181ac6d09f83248ba5ff5c0d6
DelcioCoder <armandodelcio40@gmail.com>
2024-11-13 17:58:15 -0300

    I changed the settings archive

--- backend/urls.py
+++ backend/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user_auth/', include('user_auth.urls')),
-    path('api/', include('api.urls')),  # replace 'api' with your app name  
+    path('api/', include('api.urls')), 
 ]
