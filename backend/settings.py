81764284e506074c679ac45156561c42389a0b9c
DelcioCoder <armandodelcio40@gmail.com>
2024-11-20 09:05:10 -0300

    I configured the mysql database

--- backend/settings.py
+++ backend/settings.py
@@ -116,8 +116,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'personal_backend',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost'
     }
 }
 
